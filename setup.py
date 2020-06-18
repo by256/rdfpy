e898c9789a8b9e8c349fec5fb30f4a251e00c2b2
by256 <by256@cam.ac.uk>
2020-06-18 13:10:47 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(name='rdfpy',
+    version='0.1',
+    description='rdfpy is a Python package for computing 2D and 3D radial distribution functions.',
+    author='Batuhan Yildirim',
+    author_email='by256@cam.ac.uk',
+    url='https://github.com/by256/rdfpy',
+    packages=['distutils', 'numpy', 'matplotlib'],
+    license='MIT'
+     )
