92e8b91f1dba7c8e7d021b92cc20936f12a1405c
by256 <by256@cam.ac.uk>
2020-06-18 10:59:07 +0100

    added rdf3d

--- examples/random2d.py
+++ examples/random2d.py
@@ -6,7 +6,7 @@ from rdfpy import rdf2d
 
 particles = np.load('./random2d.npy')
 
-g_r, radii, r_max = rdf2d(particles, p_radius=0.25, h=30, w=30, dr=0.125)
+g_r, radii = rdf2d(particles, p_radius=0.25, h=30, w=30, dr=0.125)
 
 
 fig, axes = plt.subplots(1, 2, figsize=(12, 4), gridspec_kw={'width_ratios': [1, 2]})
@@ -19,5 +19,5 @@ axes[1].hlines(y=1.0, xmin=0.0, xmax=max(radii), color='r', linestyle='--', alph
 axes[1].set_ylabel('g(r)')
 axes[1].set_xlabel('r')
 axes[1].set_xlim(0.0, max(radii))
-plt.savefig('./random2d.png')
-plt.show()
+plt.savefig('./random2d.png', bbox_inches='tight', pad_inches=0.25)
+# plt.show()
