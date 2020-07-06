0d1fd45c875047556d93aabd7246658e775d8a92
by256 <by256@cam.ac.uk>
2020-07-06 16:04:28 +0100

    fixed issues

---
+++ rdfpy/examples/water.py
@@ -0,0 +1,33 @@
+import sys
+sys.path.append('..')
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from rdfpy import rdf3d
+
+particles = np.load('./water.npy')
+
+fig = plt.figure(figsize=(9, 9))
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(particles[:, 0], particles[:, 1], particles[:, 2], color='aqua', alpha=0.2, edgecolors='k', s=25)
+plt.axis('off')
+plt.savefig('./water-particles.png', bbox_inches='tight', pad_inches=0.0)
+plt.close()
+
+g_r, radii = rdf3d(particles, dr=0.2)
+
+image = plt.imread('./water-particles.png')
+
+fig, axes = plt.subplots(1, 2, figsize=(12, 4), gridspec_kw={'width_ratios': [1, 2]})
+
+axes[0].imshow(image)
+axes[0].axis('off')
+
+axes[1].plot(radii, g_r, color='k', alpha=0.75)
+axes[1].hlines(y=1.0, xmin=0.0, xmax=max(radii), color='r', linestyle='--', alpha=0.4)
+axes[1].set_ylabel('g(r)')
+axes[1].set_xlabel('r')
+axes[1].set_xlim(0.0, max(radii))
+plt.suptitle('Water')
+plt.savefig('./water.png', bbox_inches='tight', pad_inches=0.25)
+plt.show()
--- rdfpy/rdfpy.py
+++ rdfpy/rdfpy.py
@@ -1,129 +1,138 @@
+import time
 import numpy as np
+from scipy.spatial import cKDTree
 
 
-def rdf2d(particles, p_radius, h, w, n_radii=None, dr=None):
-    """Computes 2D radial disribution functions (g(r)) of a set of particles of shape (N, 2).
-
-    :param particles: set of particles of shape (N, 2).
-    :type name: np.array
-    :param p_radius: radius of particles.
-    :type p_radius: float
-    :param h: height of the plane containing the particles.
-    :type h: float
-    :param w: width of the plane containing the particles.
-    :type w: float
-    :param n_radii: number of radii over which to compute g(r). Should not be specified if dr is specified.
-    :type n_radii: int
-    :param dr: \delta r. Determines the spacing between successive radii over which g(r) is computed. Should not be specified if n_radii is specified.
-    :type dr: int
-    
-    :returns:
-        - g_r (np.array) - radial distribution function values (g(r)) (dependent variable).
-        - radii (np.array) - radii over which g(r) is computed (independent variable).
+def rdf2d(particles, dr, rho=None, eps=1e-15):
     """
-
-    centre_y, centre_x = h/2, w/2
-    min_x, max_x = np.min(particles[:, 0]), np.max(particles[:, 0])
-    min_y, max_y = np.min(particles[:, 1]), np.max(particles[:, 1])
-
-    r_max = (np.min([h, w]) / 2) - 2*p_radius
-
-    if n_radii and not dr:
-        radii = np.linspace(0.0, r_max, n_radii)
-        dr = radii[1] - radii[0]
-        radii = radii + dr
-    elif dr and not n_radii:
-        radii = np.arange(dr, r_max, dr)
-    elif n_radii is not None and dr is not None:
-        raise ValueError('Arguments n_radii and dr should not both be specified.')
+    Computes 2D radial disribution functions g(r) of a set of particles of shape (N, 2).
+    Particle must be placed in a 2D box of dimensions (width x height).
     
-    particles_individual_counts = np.zeros(shape=(len(particles), len(radii)))
+    Parameters
+    ----------
+    particles : (N, 2) np.array
+        Set of particle from which to compute the radial distribution funntion g(r).
+    dr : float
+        Delta r. Determines the spacing between successive radii over which g(r) is computed.
+    rho : float, optional
+        Number density. In cases where the number of particles is relatively low, or the 
+        particle:box size ratio is small, it may be better to provide your own number density 
+        for more accurate results. If left as None, box dimensions will be inferred from the 
+        particles and the number density will be calculated accordingly.
+    eps : float, optional
+        Epsilon value used to find particles less than or equal to a distance in KDTree.
+        
+    
+    Returns
+    -------
+    g_r : (n_radii) np.array
+        radial distribution function values g(r).
+    radii : (n_radii) np.array
+        radii over which g(r) is computed
+    """
+    
+    start = time.time()
+    
+    # translate particles such that the particle with min coords is at origin
+    particles = particles - np.min(particles, axis=0)
+    min_x, min_y = np.min(particles, axis=0)
+    max_x, max_y = np.max(particles, axis=0)
+    
+    # dimensions of box
+    w, h = (max_x-min_x), (max_y-min_y)
+
+    r_max = (np.min([w, h]) / 2)*0.8
+    radii = np.arange(dr, r_max, dr)
+    g_r = np.zeros(shape=(len(radii)))
     
     N = len(particles)
-    number_density = N / (h*w)
+    if not rho:
+        rho = N / (w*h) # number density
+
+    # create a KDTree for fast nearest-neighbour lookup of particles
+    tree = cKDTree(particles)
     
     for r_idx, r in enumerate(radii):
-        valid_idxs = ((particles[:, 0] - (r-p_radius) >= min_x) & (particles[:, 0] + (r-p_radius) <= max_x)) \
-                  & (((particles[:, 1] - (r-p_radius) >= min_y) & (particles[:, 1] + (r-p_radius) <= max_y)))
+        valid_idxs = (particles[:, 0]-(r+dr) >= min_x) & (particles[:, 0]+(r+dr) <= max_x) \
+                   & (particles[:, 1]-(r+dr) >= min_y) & (particles[:, 1]+(r+dr) <= max_y)
         valid_particles = particles[valid_idxs]
-        n_valid = len(valid_particles)
         
-        for p_idx, particle in enumerate(valid_particles):
-            distances = np.linalg.norm(particle - particles, axis=1)
-            n = len(distances[(distances >= r) & (distances < r+dr)])
-            particles_individual_counts[p_idx, r_idx] = n
+        for particle in valid_particles:
+            n = tree.query_ball_point(particle, r+dr-eps, return_length=True) - tree.query_ball_point(particle, r, return_length=True)
+            g_r[r_idx] += n
+        
+        n_valid = len(valid_particles)
+        shell_vol = np.pi*((r+dr)**2 - r**2)
+        g_r[r_idx] /= n_valid*shell_vol*rho
 
-        scaling_factor = 1 / (n_valid*np.pi*((r+dr)**2 - r**2))
-        particles_individual_counts[:, r_idx] = particles_individual_counts[:, r_idx] * scaling_factor
-        print('{}/{}'.format(r_idx+1, len(radii)), end='\r', flush=True)
-
-    particles_individual_counts = particles_individual_counts / number_density
-    
-    g_r = np.sum(particles_individual_counts, axis=0)
+        print('Computing RDF     Radius {}/{}    Time elapsed: {:.3f} s'.format(r_idx+1, len(radii), time.time()-start), end='\r', flush=True)
+        
     return g_r, radii
 
 
-def rdf3d(particles, p_radius, h, w, d, n_radii=None, dr=None):
-    """Computes 3D radial disribution functions (g(r)) of a set of particles of shape (N, 3).
-
-    :param particles: set of particles of shape (N, 3).
-    :type name: np.array
-    :param p_radius: radius of particles.
-    :type p_radius: float
-    :param h: height of the box containing the particles.
-    :type h: float
-    :param w: width of the box containing the particles.
-    :type w: float
-    :param d: width of the box containing the particles.
-    :type d: float
-    :param n_radii: number of radii over which to compute g(r). Should not be specified if dr is specified.
-    :type n_radii: int
-    :param dr: \delta r. Determines the spacing between successive radii over which g(r) is computed. Should not be specified if n_radii is specified.
-    :type dr: int
-    
-    :returns:
-        - g_r (np.array) - radial distribution function values (g(r)) (dependent variable).
-        - radii (np.array) - radii over which g(r) is computed (independent variable).
+def rdf3d(particles, dr, rho=None, eps=1e-15):
     """
-
-    centre_y, centre_x, centre_z = h / 2, w / 2, d / 2
-    min_x, max_x = np.min(particles[:, 0]), np.max(particles[:, 0])
-    min_y, max_y = np.min(particles[:, 1]), np.max(particles[:, 1])
-    min_z, max_z = np.min(particles[:, 2]), np.max(particles[:, 2])
-
-    r_max = (np.min([h, w, d]) / 2) - 2*p_radius
-
-    if n_radii and not dr:
-        radii = np.linspace(0.0, r_max, n_radii)
-        dr = radii[1] - radii[0]
-        radii = radii + dr
-    elif dr and not n_radii:
-        radii = np.arange(dr, r_max, dr)
-    elif n_radii is not None and dr is not None:
-        raise ValueError('Arguments n_radii and dr should not both be specified.')
+    Computes 3D radial disribution functions g(r) of a set of particles of shape (N, 3).
+    Particle must be placed in a 3D cuboidal box of dimensions (width x height x depth).
     
-    particles_individual_counts = np.zeros(shape=(len(particles), len(radii)))
+    Parameters
+    ----------
+    particles : (N, 3) np.array
+        Set of particle from which to compute the radial distribution funntion g(r).
+    dr : float
+        Delta r. Determines the spacing between successive radii over which g(r) is computed.
+    rho : float, optional
+        Number density. In cases where the number of particles is relatively low, or the 
+        particle:box size ratio is small, it may be better to provide your own number density 
+        for more accurate results. If left as None, box dimensions will be inferred from the 
+        particles and the number density will be calculated accordingly.
+    eps : float, optional
+        Epsilon value used to find particles less than or equal to a distance in KDTree.
+        
+    
+    Returns
+    -------
+    g_r : (n_radii) np.array
+        radial distribution function values g(r).
+    radii : (n_radii) np.array
+        radii over which g(r) is computed
+    """
+    
+    start = time.time()
+    
+    # translate particles such that the particle with min coords is at origin
+    particles = particles - np.min(particles, axis=0)
+    min_x, min_y, min_z = np.min(particles, axis=0)
+    max_x, max_y, max_z = np.max(particles, axis=0)
+    
+    # dimensions of box
+    w, h, d = (max_x-min_x), (max_y-min_y), (max_z-min_z)
+
+    r_max = (np.min([w, h, d]) / 2)*0.8
+    radii = np.arange(dr, r_max, dr)
+    g_r = np.zeros(shape=(len(radii)))
     
     N = len(particles)
-    number_density = N / (h*w*d)
+    if not rho:
+        rho = N / (w*h*d) # number density
+
+    # create a KDTree for fast nearest-neighbour lookup of particles
+    tree = cKDTree(particles)
     
     for r_idx, r in enumerate(radii):
-        valid_idxs = ((particles[:, 0] - (r-p_radius) >= min_x) & (particles[:, 0] + (r-p_radius) <= max_x)) \
-                  & (((particles[:, 1] - (r-p_radius) >= min_y) & (particles[:, 1] + (r-p_radius) <= max_y))) \
-                  & (((particles[:, 2] - (r-p_radius) >= min_z) & (particles[:, 2] + (r-p_radius) <= max_z)))
+        valid_idxs = (particles[:, 0]-(r+dr) >= min_x) & (particles[:, 0]+(r+dr) <= max_x) \
+                   & (particles[:, 1]-(r+dr) >= min_y) & (particles[:, 1]+(r+dr) <= max_y) \
+                   & (particles[:, 2]-(r+dr) >= min_z) & (particles[:, 2]+(r+dr) <= max_z)
         valid_particles = particles[valid_idxs]
-        n_valid = len(valid_particles)
         
-        for p_idx, particle in enumerate(valid_particles):
-            distances = np.linalg.norm(particle - particles, axis=1)
-            n = len(distances[(distances >= r) & (distances < r+dr)])
-            particles_individual_counts[p_idx, r_idx] = n
+        for particle in valid_particles:
+            n = tree.query_ball_point(particle, r+dr-eps, return_length=True) - tree.query_ball_point(particle, r, return_length=True)
+            g_r[r_idx] += n
+        
+        n_valid = len(valid_particles)
+        shell_vol = (4/3)*np.pi*((r+dr)**3 - r**3)
+        g_r[r_idx] /= n_valid*shell_vol*rho
 
-        scaling_factor = 1 / (n_valid*(4/3)*np.pi*((r+dr)**3 - r**3))
-        particles_individual_counts[:, r_idx] = particles_individual_counts[:, r_idx] * scaling_factor
-        print('{}/{}'.format(r_idx+1, len(radii)), end='\r', flush=True)
-
-    particles_individual_counts = particles_individual_counts / number_density
-    
-    g_r = np.sum(particles_individual_counts, axis=0)
+        print('Computing RDF     Radius {}/{}    Time elapsed: {:.3f} s'.format(r_idx+1, len(radii), time.time()-start), end='\r', flush=True)
+        
     return g_r, radii
