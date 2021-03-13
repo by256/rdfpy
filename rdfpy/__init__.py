453e001215c8b70b7537640b45b87f19ccbf6302
by256 <by256@cam.ac.uk>
2021-03-13 11:18:46 +0000

    change imports such that multiprocessing is not used if system is Windows

--- rdfpy/__init__.py
+++ rdfpy/__init__.py
@@ -1,1 +1,6 @@
-from .rdfpy import rdf2d, rdf3d
+import os
+if os.name == 'nt':  # check for windows
+    from .rdfpy import rdf2d, rdf3d
+else:
+    from .rdfpy import rdf2d_parallel as rdf2d
+    from .rdfpy import rdf3d_parallel as rdf3d
