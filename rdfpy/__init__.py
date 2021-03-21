d67176476eb927a7e737918b34c26d0811d23e58
by256 <by256@cam.ac.uk>
2021-03-21 16:05:25 +0000

    alias imports for backwards compatibility

--- rdfpy/__init__.py
+++ rdfpy/__init__.py
@@ -1,6 +1,4 @@
 import os
-if os.name == 'nt':  # check for windows
-    from .rdfpy import rdf2d, rdf3d
-else:
-    from .rdfpy import rdf2d_parallel as rdf2d
-    from .rdfpy import rdf3d_parallel as rdf3d
+from .rdfpy import rdf
+from .rdfpy import rdf as rdf2d
+from .rdfpy import rdf as rdf3d
