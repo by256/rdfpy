ab402b2bd29e6d51dafb96499c4a83e481961150
by256 <by256@cam.ac.uk>
2021-03-22 10:39:49 +0000

    change docs version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Batuhan Yildirim'
 author = 'Batuhan Yildirim'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.4'
+release = '1.0.0'
 
 
 # -- General configuration ---------------------------------------------------
